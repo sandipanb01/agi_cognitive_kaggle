a8cb94ed2ee3c5da34424cbace514e97b48f7992
sandipanb01 <sandipanb12@gmail.com>
2026-03-22 00:45:26 +0530

    Create Env.py

---
+++ Env.py
@@ -0,0 +1,19 @@
+!pip install transformers\
+    tokenizers \
+    datasets\
+    sacrebleu\
+    torch\
+    torchvision\
+    tqdm \
+    peft\
+    bitsandbytes \
+    accelerate\
+    sentencepiece\
+    lxml\
+    pandas\
+    matplotlib\
+    xlsxwriter\
+    evaluate\
+    trl
+!pip install --upgrade torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+!pip install transformers tqdm matplotlib pandas
